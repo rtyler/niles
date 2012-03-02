fc1e4bdf1157f615b71131c932b27b72b8c611bb
R. Tyler Croy <tyler@monkeypox.org>
2012-03-01 23:49:06 -0800

    Create a Job object in the Niles package to make it easier to manage Job data
    I figure Job will also offer up an #asPage method which will generate a JQPage

--- js/Niles.deploy.js
+++ js/Niles.deploy.js
@@ -1,4 +1,52 @@
 smalltalk.addPackage('Niles', {});
+smalltalk.addClass('Job', smalltalk.Object, ['description', 'url', 'buildable', 'stable'], 'Niles');
+smalltalk.addMethod(
+unescape('_initialize'),
+smalltalk.method({
+selector: unescape('initialize'),
+fn: function (){
+var self=this;
+(name="");
+(self['@description']="");
+(self['@url']="");
+(self['@buildable']=false);
+(self['@stable']=false);
+return self;}
+}),
+smalltalk.Job);
+
+smalltalk.addMethod(
+unescape('_fromDictionary_'),
+smalltalk.method({
+selector: unescape('fromDictionary%3A'),
+fn: function (aDictionary){
+var self=this;
+(name=smalltalk.send(aDictionary, "_name", []));
+(self['@url']=smalltalk.send(aDictionary, "_url", []));
+(self['@description']=smalltalk.send(aDictionary, "_description", []));
+(self['@buildable']=smalltalk.send(aDictionary, "_buildable", []));
+((($receiver = smalltalk.send(smalltalk.send(aDictionary, "_color", []), "__eq", ["blue"])).klass === smalltalk.Boolean) ? ($receiver ? (function(){return (self['@stable']=true);})() : nil) : smalltalk.send($receiver, "_ifTrue_", [(function(){return (self['@stable']=true);})]));
+return self;}
+}),
+smalltalk.Job);
+
+smalltalk.addMethod(
+unescape('_asListItem'),
+smalltalk.method({
+selector: unescape('asListItem'),
+fn: function (){
+var self=this;
+var item=nil;
+(item=(function($rec){smalltalk.send($rec, "_withLabel_", [(typeof name == 'undefined' ? nil : name)]);smalltalk.send($rec, "_withHref_", [self['@url']]);return smalltalk.send($rec, "_withOnClick_", [(function(event){smalltalk.send((typeof window == 'undefined' ? nil : window), "_alert_", [smalltalk.send("Navigating to: ", "__comma", [(typeof name == 'undefined' ? nil : name)])]);return smalltalk.send(event, "_preventDefault", []);})]);})(smalltalk.send((smalltalk.JQListItem || JQListItem), "_new", [])));
+((($receiver = self['@stable']).klass === smalltalk.Boolean) ? (! $receiver ? (function(){return smalltalk.send(item, "_turnYellow", []);})() : nil) : smalltalk.send($receiver, "_ifFalse_", [(function(){return smalltalk.send(item, "_turnYellow", []);})]));
+((($receiver = ((($receiver = smalltalk.send(self['@description'], "_size", [])).klass === smalltalk.Number) ? $receiver >(0) : smalltalk.send($receiver, "__gt", [(0)]))).klass === smalltalk.Boolean) ? ($receiver ? (function(){return smalltalk.send(item, "_withDescription_", [self['@description']]);})() : nil) : smalltalk.send($receiver, "_ifTrue_", [(function(){return smalltalk.send(item, "_withDescription_", [self['@description']]);})]));
+return item;
+return self;}
+}),
+smalltalk.Job);
+
+
+
 smalltalk.addClass('NilesApp', smalltalk.Object, ['list'], 'Niles');
 smalltalk.addMethod(
 unescape('_clear'),
@@ -21,7 +69,8 @@ var self=this;
 smalltalk.send(self, "_clear", []);
 (($receiver = self['@list']) == nil || $receiver == undefined) ? (function(){(self['@list']=smalltalk.send((smalltalk.JQList || JQList), "_new", []));return smalltalk.send(self['@list'], "_appendToJQuery_", [smalltalk.send(unescape("%23niles-content"), "_asJQuery", [])]);})() : $receiver;
 smalltalk.send((smalltalk.Jenkins || Jenkins), "_fetchFor_depth_onSuccess_onError_", [aUrl, (1), (function(data){return smalltalk.send(smalltalk.send(data, "_jobs", []), "_do_", [(function(job){var item=nil;
-(item=(function($rec){smalltalk.send($rec, "_withLabel_", [smalltalk.send(job, "_name", [])]);smalltalk.send($rec, "_withHref_", [smalltalk.send(job, "_url", [])]);return smalltalk.send($rec, "_withOnClick_", [(function(event){smalltalk.send((typeof window == 'undefined' ? nil : window), "_alert_", [smalltalk.send("Navigating to: ", "__comma", [smalltalk.send(job, "_name", [])])]);return smalltalk.send(event, "_preventDefault", []);})]);})(smalltalk.send((smalltalk.JQListItem || JQListItem), "_new", [])));((($receiver = smalltalk.send(smalltalk.send(job, "_color", []), "__eq", ["yellow"])).klass === smalltalk.Boolean) ? ($receiver ? (function(){return smalltalk.send(item, "_turnYellow", []);})() : nil) : smalltalk.send($receiver, "_ifTrue_", [(function(){return smalltalk.send(item, "_turnYellow", []);})]));((($receiver = ((($receiver = smalltalk.send(smalltalk.send(job, "_description", []), "_size", [])).klass === smalltalk.Number) ? $receiver >(0) : smalltalk.send($receiver, "__gt", [(0)]))).klass === smalltalk.Boolean) ? ($receiver ? (function(){return smalltalk.send(item, "_withDescription_", [smalltalk.send(job, "_description", [])]);})() : nil) : smalltalk.send($receiver, "_ifTrue_", [(function(){return smalltalk.send(item, "_withDescription_", [smalltalk.send(job, "_description", [])]);})]));return smalltalk.send(self['@list'], "_addItem_", [item]);})]);}), (function(){return smalltalk.send((typeof window == 'undefined' ? nil : window), "_alert_", ["Fucking fuck"]);})]);
+var njob=nil;
+(njob=smalltalk.send(smalltalk.send((smalltalk.Job || Job), "_new", []), "_fromDictionary_", [job]));return smalltalk.send(self['@list'], "_addItem_", [smalltalk.send(njob, "_asListItem", [])]);})]);}), (function(){return smalltalk.send((typeof window == 'undefined' ? nil : window), "_alert_", ["Fucking fuck"]);})]);
 return self;}
 }),
 smalltalk.NilesApp);
--- js/Niles.js
+++ js/Niles.js
@@ -1,4 +1,67 @@
 smalltalk.addPackage('Niles', {});
+smalltalk.addClass('Job', smalltalk.Object, ['description', 'url', 'buildable', 'stable'], 'Niles');
+smalltalk.addMethod(
+unescape('_initialize'),
+smalltalk.method({
+selector: unescape('initialize'),
+category: 'not yet classified',
+fn: function (){
+var self=this;
+(name="");
+(self['@description']="");
+(self['@url']="");
+(self['@buildable']=false);
+(self['@stable']=false);
+return self;},
+args: [],
+source: unescape('initialize%0A%09name%20%3A%3D%20%27%27.%0A%09description%20%3A%3D%20%27%27.%0A%09url%20%3A%3D%20%27%27.%0A%09buildable%20%3A%3D%20false.%0A%09stable%20%3A%3D%20false.'),
+messageSends: [],
+referencedClasses: []
+}),
+smalltalk.Job);
+
+smalltalk.addMethod(
+unescape('_fromDictionary_'),
+smalltalk.method({
+selector: unescape('fromDictionary%3A'),
+category: 'not yet classified',
+fn: function (aDictionary){
+var self=this;
+(name=smalltalk.send(aDictionary, "_name", []));
+(self['@url']=smalltalk.send(aDictionary, "_url", []));
+(self['@description']=smalltalk.send(aDictionary, "_description", []));
+(self['@buildable']=smalltalk.send(aDictionary, "_buildable", []));
+((($receiver = smalltalk.send(smalltalk.send(aDictionary, "_color", []), "__eq", ["blue"])).klass === smalltalk.Boolean) ? ($receiver ? (function(){return (self['@stable']=true);})() : nil) : smalltalk.send($receiver, "_ifTrue_", [(function(){return (self['@stable']=true);})]));
+return self;},
+args: ["aDictionary"],
+source: unescape('fromDictionary%3A%20aDictionary%0A%09name%20%3A%3D%20aDictionary%20name.%0A%09url%20%3A%3D%20aDictionary%20url.%0A%09description%20%3A%3D%20aDictionary%20description.%0A%09buildable%20%3A%3D%20aDictionary%20buildable.%0A%09aDictionary%20color%20%3D%20%27blue%27%0A%09%09ifTrue%3A%20%5B%20stable%20%3A%3D%20true%20%5D.'),
+messageSends: ["name", "url", "description", "buildable", "ifTrue:", unescape("%3D"), "color"],
+referencedClasses: []
+}),
+smalltalk.Job);
+
+smalltalk.addMethod(
+unescape('_asListItem'),
+smalltalk.method({
+selector: unescape('asListItem'),
+category: 'not yet classified',
+fn: function (){
+var self=this;
+var item=nil;
+(item=(function($rec){smalltalk.send($rec, "_withLabel_", [(typeof name == 'undefined' ? nil : name)]);smalltalk.send($rec, "_withHref_", [self['@url']]);return smalltalk.send($rec, "_withOnClick_", [(function(event){smalltalk.send((typeof window == 'undefined' ? nil : window), "_alert_", [smalltalk.send("Navigating to: ", "__comma", [(typeof name == 'undefined' ? nil : name)])]);return smalltalk.send(event, "_preventDefault", []);})]);})(smalltalk.send((smalltalk.JQListItem || JQListItem), "_new", [])));
+((($receiver = self['@stable']).klass === smalltalk.Boolean) ? (! $receiver ? (function(){return smalltalk.send(item, "_turnYellow", []);})() : nil) : smalltalk.send($receiver, "_ifFalse_", [(function(){return smalltalk.send(item, "_turnYellow", []);})]));
+((($receiver = ((($receiver = smalltalk.send(self['@description'], "_size", [])).klass === smalltalk.Number) ? $receiver >(0) : smalltalk.send($receiver, "__gt", [(0)]))).klass === smalltalk.Boolean) ? ($receiver ? (function(){return smalltalk.send(item, "_withDescription_", [self['@description']]);})() : nil) : smalltalk.send($receiver, "_ifTrue_", [(function(){return smalltalk.send(item, "_withDescription_", [self['@description']]);})]));
+return item;
+return self;},
+args: [],
+source: unescape('asListItem%0A%09%22Return%20a%20JQListItem%20representing%20this%20Job%20object%22%0A%09%7C%20item%20%7C%0A%09item%20%3A%3D%20JQListItem%20new%0A%09%09%09withLabel%3A%20name%3B%0A%09%09%09withHref%3A%20url%3B%0A%09%09%09withOnClick%3A%20%5B%20%3Aevent%20%7C%20window%20alert%3A%20%28%27Navigating%20to%3A%20%27%2C%20%20name%29.%20event%20preventDefault%20%5D.%0A%09stable%20ifFalse%3A%20%5B%20item%20turnYellow%20%5D.%0A%09description%20size%20%3E%200%0A%09%09%09%09ifTrue%3A%20%5B%20item%20withDescription%3A%20description%20%5D.%0A%09%5E%20item.'),
+messageSends: ["withLabel:", "withHref:", "withOnClick:", "alert:", unescape("%2C"), "preventDefault", "new", "ifFalse:", "turnYellow", "ifTrue:", unescape("%3E"), "size", "withDescription:"],
+referencedClasses: ["JQListItem"]
+}),
+smalltalk.Job);
+
+
+
 smalltalk.addClass('NilesApp', smalltalk.Object, ['list'], 'Niles');
 smalltalk.addMethod(
 unescape('_clear'),
@@ -27,12 +90,13 @@ var self=this;
 smalltalk.send(self, "_clear", []);
 (($receiver = self['@list']) == nil || $receiver == undefined) ? (function(){(self['@list']=smalltalk.send((smalltalk.JQList || JQList), "_new", []));return smalltalk.send(self['@list'], "_appendToJQuery_", [smalltalk.send(unescape("%23niles-content"), "_asJQuery", [])]);})() : $receiver;
 smalltalk.send((smalltalk.Jenkins || Jenkins), "_fetchFor_depth_onSuccess_onError_", [aUrl, (1), (function(data){return smalltalk.send(smalltalk.send(data, "_jobs", []), "_do_", [(function(job){var item=nil;
-(item=(function($rec){smalltalk.send($rec, "_withLabel_", [smalltalk.send(job, "_name", [])]);smalltalk.send($rec, "_withHref_", [smalltalk.send(job, "_url", [])]);return smalltalk.send($rec, "_withOnClick_", [(function(event){smalltalk.send((typeof window == 'undefined' ? nil : window), "_alert_", [smalltalk.send("Navigating to: ", "__comma", [smalltalk.send(job, "_name", [])])]);return smalltalk.send(event, "_preventDefault", []);})]);})(smalltalk.send((smalltalk.JQListItem || JQListItem), "_new", [])));((($receiver = smalltalk.send(smalltalk.send(job, "_color", []), "__eq", ["yellow"])).klass === smalltalk.Boolean) ? ($receiver ? (function(){return smalltalk.send(item, "_turnYellow", []);})() : nil) : smalltalk.send($receiver, "_ifTrue_", [(function(){return smalltalk.send(item, "_turnYellow", []);})]));((($receiver = ((($receiver = smalltalk.send(smalltalk.send(job, "_description", []), "_size", [])).klass === smalltalk.Number) ? $receiver >(0) : smalltalk.send($receiver, "__gt", [(0)]))).klass === smalltalk.Boolean) ? ($receiver ? (function(){return smalltalk.send(item, "_withDescription_", [smalltalk.send(job, "_description", [])]);})() : nil) : smalltalk.send($receiver, "_ifTrue_", [(function(){return smalltalk.send(item, "_withDescription_", [smalltalk.send(job, "_description", [])]);})]));return smalltalk.send(self['@list'], "_addItem_", [item]);})]);}), (function(){return smalltalk.send((typeof window == 'undefined' ? nil : window), "_alert_", ["Fucking fuck"]);})]);
+var njob=nil;
+(njob=smalltalk.send(smalltalk.send((smalltalk.Job || Job), "_new", []), "_fromDictionary_", [job]));return smalltalk.send(self['@list'], "_addItem_", [smalltalk.send(njob, "_asListItem", [])]);})]);}), (function(){return smalltalk.send((typeof window == 'undefined' ? nil : window), "_alert_", ["Fucking fuck"]);})]);
 return self;},
 args: ["aUrl"],
-source: unescape('load%3A%20aUrl%0A%09self%20clear.%0A%09list%20ifNil%3A%20%5B%20%0A%09%09list%20%3A%3D%20JQList%20new.%0A%09%09list%20appendToJQuery%3A%20%27%23niles-content%27%20asJQuery%5D.%0A%0A%09Jenkins%20fetchFor%3A%20aUrl%0A%09%09depth%3A%201%0A%09%09onSuccess%3A%20%5B%20%3Adata%20%7C%20data%20jobs%20do%3A%20%5B%20%3Ajob%20%7C%0A%09%09%09%7C%20item%20%7C%0A%09%09%09item%20%3A%3D%20JQListItem%20new%0A%09%09%09%09%09withLabel%3A%20job%20name%3B%0A%09%09%09%09%09withHref%3A%20job%20url%3B%0A%09%09%09%09%09withOnClick%3A%20%5B%20%3Aevent%20%7C%20window%20alert%3A%20%28%27Navigating%20to%3A%20%27%2C%20job%20name%29.%20event%20preventDefault%20%5D.%0A%09%09%09job%20color%20%3D%20%27yellow%27%20ifTrue%3A%20%5B%20item%20turnYellow%20%5D.%0A%09%09%09job%20description%20size%20%3E%200%0A%09%09%09%09ifTrue%3A%20%5B%20item%20withDescription%3A%20job%20description%20%5D.%0A%09%09%09%22%28job%20healthReport%20size%29%20%3E%200%0A%09%09%09%09ifTrue%3A%20%5Bitem%20addIcon%3A%20%27http%3A//ci.jenkins-ci.org/images/48x48/%27%2C%20%28%28job%20healthReport%20at%3A%201%29%20at%3A%20%27iconUrl%27%29%5D.%0A%09%09%09%22list%20addItem%3A%20item.%5D%5D%0A%09%09onError%3A%20%5B%20window%20alert%3A%20%27Fucking%20fuck%27%20%5D.'),
-messageSends: ["clear", "ifNil:", "new", "appendToJQuery:", "asJQuery", "fetchFor:depth:onSuccess:onError:", "do:", "jobs", "withLabel:", "name", "withHref:", "url", "withOnClick:", "alert:", unescape("%2C"), "preventDefault", "ifTrue:", unescape("%3D"), "color", "turnYellow", unescape("%3E"), "size", "description", "withDescription:", "addItem:"],
-referencedClasses: ["JQList", "Jenkins", "JQListItem"]
+source: unescape('load%3A%20aUrl%0A%09self%20clear.%0A%09list%20ifNil%3A%20%5B%20%0A%09%09list%20%3A%3D%20JQList%20new.%0A%09%09list%20appendToJQuery%3A%20%27%23niles-content%27%20asJQuery%5D.%0A%0A%09Jenkins%20fetchFor%3A%20aUrl%0A%09%09depth%3A%201%0A%09%09onSuccess%3A%20%5B%20%3Adata%20%7C%20data%20jobs%20do%3A%20%5B%20%3Ajob%20%7C%0A%09%09%09%7C%20item%20njob%20%7C%0A%09%09%09njob%20%3A%3D%20Job%20new%20fromDictionary%3A%20job.%0A%09%09%09list%20addItem%3A%20njob%20asListItem.%5D%5D%0A%09%09onError%3A%20%5B%20window%20alert%3A%20%27Fucking%20fuck%27%20%5D.'),
+messageSends: ["clear", "ifNil:", "new", "appendToJQuery:", "asJQuery", "fetchFor:depth:onSuccess:onError:", "do:", "jobs", "fromDictionary:", "addItem:", "asListItem", "alert:"],
+referencedClasses: ["JQList", "Jenkins", "Job"]
 }),
 smalltalk.NilesApp);
 
